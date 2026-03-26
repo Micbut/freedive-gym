6f08bc54ebae4b0287ccee5e25dd5a2ec045af0b
Michele Butturini <michelebutturini@MacBookPro.home>
2026-03-26 22:49:47 +0100

    Force service worker cache bust, ensure fresh content loads

--- sw.js
+++ sw.js
@@ -1,9 +1,13 @@
-const CACHE = 'freedive-gym-v8';
+const CACHE = 'freedive-gym-v9';
 const ASSETS = ['./', 'index.html', 'manifest.json', 'icon-192.png', 'icon-512.png'];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
   self.skipWaiting();
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => caches.delete(k)))
+    ).then(() => caches.open(CACHE).then(c => c.addAll(ASSETS)))
+  );
 });
 
 self.addEventListener('activate', e => {
