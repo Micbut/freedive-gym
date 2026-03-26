b66f0aa3e54182b72f84cdf6c25ed35eda925a97
Michele Butturini <michelebutturini@MacBookPro.home>
2026-03-26 22:41:48 +0100

    Show install instructions for all browsers: Chrome, Safari, Edge

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'freedive-gym-v5';
+const CACHE = 'freedive-gym-v6';
 const ASSETS = ['./', 'index.html', 'manifest.json', 'icon-192.png', 'icon-512.png'];
 
 self.addEventListener('install', e => {
