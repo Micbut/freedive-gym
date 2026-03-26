14bbe2377c7a2889a68678579605d596b07e48dc
Michele Butturini <michelebutturini@MacBookPro.home>
2026-03-26 22:31:29 +0100

    Add home screen with 4 large navigation buttons, back button

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'freedive-gym-v2';
+const CACHE = 'freedive-gym-v3';
 const ASSETS = ['./', 'index.html', 'manifest.json', 'icon-192.png', 'icon-512.png'];
 
 self.addEventListener('install', e => {
