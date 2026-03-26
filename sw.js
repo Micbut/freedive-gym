cdb44ab8b0cdc6eaf262a04c3b902bd4f25a8eaf
Michele Butturini <michelebutturini@MacBookPro.home>
2026-03-26 23:22:13 +0100

    Remove browser install hint from footer, bump cache v12

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'freedive-gym-v11';
+const CACHE = 'freedive-gym-v12';
 const ASSETS = ['./', 'index.html', 'manifest.json', 'icon-192.png', 'icon-512.png'];
 
 self.addEventListener('install', e => {
