e486381c571ac3b8b32f227e56eb40cdde33be24
Michele Butturini <michelebutturini@MacBookPro.home>
2026-03-26 22:44:10 +0100

    Always show install footer in browser, no delay or detection issues

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'freedive-gym-v6';
+const CACHE = 'freedive-gym-v7';
 const ASSETS = ['./', 'index.html', 'manifest.json', 'icon-192.png', 'icon-512.png'];
 
 self.addEventListener('install', e => {
